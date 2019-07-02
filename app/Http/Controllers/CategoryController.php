dbc98fada306972d6b52c094acda08cf17952aed
gancarajriya <gancarajriya@gmail.com>
2019-07-02 09:51:08 +0700

    fitur add kategori

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,4 +13,23 @@ class CategoryController extends Controller
         return view('categories.index', compact('categories'));
     }
 
+    public function store(Request $request)
+    {
+        //validasi form
+        $this->validate($request, [
+            'name' => 'required|string|max:50',
+            'description' => 'nullable|string'
+        ]);
+        try {
+            $categories = Category::firstOrCreate([
+                'name' => $request->name
+            ], [
+                'description' => $request->description
+            ]);
+            return redirect()->back()->with(['success' => 'Kategori: ' . $categories->name . ' Ditambahkan']);
+        } catch (\Exception $e) {
+            return redirect()->back()->with(['error' => $e->getMessage()]);
+        }
+    }
+
 }
